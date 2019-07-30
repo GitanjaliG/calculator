af8ec929f68127dd6b7b2def7c60d476a488587c
Gitanjali <gitanjali@Gitanjali.local>
2019-07-30 19:38:43 +0530

    v1.1

--- src/index.js
+++ src/index.js
@@ -4,9 +4,96 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Display(props){
+    return(
+      <input type="text" className="displayarea" value={props.value} disabled/>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Key(props) {
+  return (
+    <button className="key" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component{
+  constructor(props) {
+  super(props);
+    this.state = {
+      key: 0,
+    };
+  }
+
+  renderKey(i) {
+   return (
+     <Key
+       value={[i]}
+       onClick={() =>  this.handleClick(i)}
+     />
+   );
+ }
+ renderDisplay(i) {
+  return (
+    <Display
+      value={this.state.key}
+    />
+  );
+}
+ handleClick(i,calculate){
+   let key;
+   if (i === "=") {
+     let result = this.state.key;
+      key = eval(result);
+   }else {
+      key = (this.state.key +i).toString();
+   }
+   this.setState({
+     key:key
+   });
+ }
+  render(){
+    return(
+      <div className="wrapper">
+        {this.renderDisplay()}
+        <div className="board-row">
+          {this.renderKey(7)}
+          {this.renderKey(8)}
+          {this.renderKey(9)}
+          {this.renderKey('/')}
+        </div>
+        <div className="board-row">
+          {this.renderKey(4)}
+          {this.renderKey(5)}
+          {this.renderKey(6)}
+          {this.renderKey('*')}
+        </div>
+        <div className="board-row">
+          {this.renderKey(1)}
+          {this.renderKey(2)}
+          {this.renderKey(3)}
+          {this.renderKey('-')}
+        </div>
+        <div className="board-row">
+          {this.renderKey(0)}
+          {this.renderKey('.')}
+          {this.renderKey('=')}
+          {this.renderKey('+')}
+        </div>
+      </div>
+    );
+  }
+}
+
+class  Calculator extends React.Component {
+  render(){
+    return(
+      <div className="calc">
+        <Board />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
